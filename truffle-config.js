76ad7a60ca73caf610e82f671b097f2e33360c55
Javier Tarazaga <javier.tarazaga@valtech.se>
2019-12-02 22:33:14 +0100

    Use Rinkeby instead of Ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,15 @@
 const path = require("path");
 
 const SuperblocksProvider = require("super-web3-provider").default;
-const superProvider = new SuperblocksProvider({ 
+const ropstenSuperProvider = new SuperblocksProvider({ 
   from: '0xEA6630F5bfA193f76cfc5F530648061b070e7DAd', 
   endpoint: 'https://ropsten.infura.io/v3/14a9bebf5c374938b2476abe29ca5564',
   networkId: '3'});
+
+const rinkebySuperProvider = new SuperblocksProvider({ 
+  from: '0xEA6630F5bfA193f76cfc5F530648061b070e7DAd', 
+  endpoint: 'https://rinkeby.infura.io/v3/14a9bebf5c374938b2476abe29ca5564',
+  networkId: '4'});
 
 
 module.exports = {
@@ -19,7 +24,16 @@ module.exports = {
         provider: () => {
             // We have created outside this function as we Truffle will call this function multiple times... and we don't want to create multiple 
             // instances of the provider in that case
-            return superProvider;
+            return ropstenSuperProvider;
+        },
+        from: "0xEA6630F5bfA193f76cfc5F530648061b070e7DAd", // default address to use for any transaction Truffle makes during migrations
+        network_id: '3'
+    },
+    rinkeby_metamask: {
+        provider: () => {
+            // We have created outside this function as we Truffle will call this function multiple times... and we don't want to create multiple 
+            // instances of the provider in that case
+            return rinkebySuperProvider;
         },
         from: "0xEA6630F5bfA193f76cfc5F530648061b070e7DAd", // default address to use for any transaction Truffle makes during migrations
         network_id: '3'
